01c3b125ea693199f1e616a1b83707df47153b4a
Raphael Melo <rafael.dks10@gmail.com>
2022-11-14 17:03:53 -0300

    static files and global templates have been renamed
    new names: base_static and base_templates

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'base_static'
 ]
 
 # Default primary key field type
