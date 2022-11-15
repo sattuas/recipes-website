980bcc4e6f412461209cbdb137e19da52e8aeb9a
Raphael Melo <rafael.dks10@gmail.com>
2022-11-14 23:17:34 -0300

    creating a template to view a recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    # the <int:id> will receive a value according to the parameter of the recipe function in views.py
     path('recipes/<int:id>/', views.recipe),
 ]
