7b7f7391d9caa8a823a1b817a6d010d601080a5a
Raphael Melo <rafael.dks10@gmail.com>
2022-11-16 14:26:31 -0300

    using the model recipe in the home view
    a url was created that filters recipes by category

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     # the <int:id> will receive a value according to the parameter of the recipe function in views.py
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
 ]
