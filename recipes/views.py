442af887b1ab598eb639cad0efe2ad6b961564fe
Raphael Melo <rafael.dks10@gmail.com>
2022-11-16 21:03:51 -0300

    hiding recipes that are not published
    and html file for category page

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,9 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes
@@ -13,7 +15,8 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True
     ).order_by('-id')
 
     return render(request, 'recipes/pages/home.html', context={
