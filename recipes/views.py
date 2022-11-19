736e57222b9f3b718db2ce41c64bbc74c4f5f33a
Raphael Melo <rafael.dks10@gmail.com>
2022-11-19 11:10:48 -0300

    showing recipe details page

--- recipes/views.py
+++ recipes/views.py
@@ -28,7 +28,13 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id,
+        is_published=True
+    ).order_by('-id').first()
+
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
