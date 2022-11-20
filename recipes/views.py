208902ff32d8d88d334e7ea56a00197b80685e0d
Raphael Melo <rafael.dks10@gmail.com>
2022-11-20 10:23:36 -0300

    get_object_or_404 in recipe view

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -28,10 +28,11 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
+    recipe = get_object_or_404(
+        Recipe,
         id=id,
         is_published=True
-    ).order_by('-id').first()
+    )
 
 
     return render(request, 'recipes/pages/recipe-view.html', context={
