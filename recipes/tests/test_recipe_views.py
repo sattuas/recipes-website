6ea51b5b3c0f369a0da174db4df16920f5083666
Raphael Melo <rafael.dks10@gmail.com>
2022-11-22 14:11:10 -0300

    testing the context and content

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -47,6 +47,13 @@ class RecipeViewTest(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        self.assertIn('Recipe title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
