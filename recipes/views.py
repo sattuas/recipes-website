81c41da22b519a3f24749e6d5a6c963f1718f580
Raphael Melo <rafael.dks10@gmail.com>
2022-11-23 12:53:34 -0300

    searching only published recipes

--- recipes/views.py
+++ recipes/views.py
@@ -50,8 +50,13 @@ def search(request):
         raise Http404()
 
     recipes = Recipe.objects.filter(
-        Q(title__icontains=search_term) | Q(description__icontains=search_term),
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
     ).order_by('-id')
+    
 
     return render(request, 'recipes/pages/search.html', context={
         'page_title': f'Search for "{search_term}" |',
