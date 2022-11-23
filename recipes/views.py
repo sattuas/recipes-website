93cda0bcda27b24c9c1b26239c0fe50fd69de735
Raphael Melo <rafael.dks10@gmail.com>
2022-11-23 10:04:07 -0300

    icontains and complex queries with Q

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.db.models import Q
 from django.http.response import Http404
 
 
@@ -46,9 +47,14 @@ def search(request):
     search_term = request.GET.get('q', '').strip()
 
     if not search_term:
-        raise Http404
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) | Q(description__icontains=search_term),
+    ).order_by('-id')
 
     return render(request, 'recipes/pages/search.html', context={
         'page_title': f'Search for "{search_term}" |',
-        'search_term': search_term
+        'search_term': search_term,
+        'recipes': recipes
     })
