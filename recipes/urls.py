49bc5d2fe3b152d922dc077492b0e96e82d18059
Raphael Melo <rafael.dks10@gmail.com>
2022-11-23 08:23:31 -0300

    creating search url

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,6 +7,7 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name="home"),
     # the <int:id> will receive a value according to the parameter of the recipe function in views.py
+    path('recipes/search/', views.search, name="search"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
     path('recipes/category/<int:category_id>/', views.category, name="category"),
 ]
